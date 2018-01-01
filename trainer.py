2603dc9086b4fa1c36718fc8c3c817d329169ed3
matken11235 <matken11235@gmail.com>
2018-01-01 10:17:23 +0900

    動かない

--- trainer.py
+++ trainer.py
@@ -3,9 +3,26 @@
 import tensorflow as tf
 from tensorflow.python.keras import backend as K
 import numpy as np
+from tensorflow.contrib.learn.python.learn.datasets import mnist
 # import pylab
 
 DUMP_NUM = 100
+
+class Hoge(tf.train.SessionRunHook):
+	def begin(self):
+		self._step = -1
+
+	def before_run(self, run_context):
+		self._step += 1
+		self._start_time = time.time()
+		return tf.train.SessionRunArgs(add)
+
+	def after_run(self, run_context, run_values):
+		duration = time.time() - self._start_time
+		result = run_values.results
+		if self._step % 10 == 0:
+			format_str = 'RESULT: {}, STEP:{}, {:%Y-%m-%d %H:%M:%S}, {:.2}'
+			print(format_str.format(result, self._step, datetime.now(), duration))
 
 class Trainer(object):
 	def __init__(self, gen, dis):
@@ -26,8 +43,9 @@ class Trainer(object):
 			true のときプロットを行う.
 		:return:
 		"""
+		data = mnist.read_data_sets("./", one_hot=True)
+
 		# Generate Noise
-		# z = tf.random_uniform([batch_size, self.z_dim], minval=-1, maxval=1)
 		z = tf.placeholder(tf.float32, shape=(batch_size, self.z_dim))
 		# define TF graph
 		x = self.gen(z)
@@ -38,36 +56,56 @@ class Trainer(object):
 		dis_loss = tf.losses.softmax_cross_entropy(tf.ones(shape=(batch_size, 2), dtype=tf.int32), y_pred1)
 		# dis_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf.ones(shape=(batch_size, 2), dtype=tf.float32), logits=y_pred1))
 
-		x_data = tf.placeholder(tf.float32, shape=(batch_size, 96, 96, 3))
+		x_data = tf.placeholder(tf.float32, shape=(batch_size, 28, 28, 1))
 		y_pred2 = self.dis(x_data)
 
 		dis_loss += tf.losses.softmax_cross_entropy(tf.zeros(shape=(batch_size, 2), dtype=tf.int32), y_pred2)
 		# dis_loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=tf.zeros(shape=(batch_size, 2), dtype=tf.float32), logits=y_pred2))
 
-		gen_train_step = tf.train.AdamOptimizer(0.05).minimize(gen_loss)
-		dis_train_step = tf.train.AdamOptimizer(0.05).minimize(dis_loss)
+		gen_train_step = tf.train.AdamOptimizer(0.001).minimize(gen_loss)
+		dis_train_step = tf.train.AdamOptimizer(0.001).minimize(dis_loss)
 
 		init = tf.global_variables_initializer()
-		next_element = X.get_next()
+		# next_element = X.get_next()
+
+		num_train = data.train.num_examples
 
 		with tf.Session() as sess:
-			sess.run(init)
+			sess.run(tf.group(init, tf.local_variables_initializer()))
 			for epoch in range(epochs):
-				sess.run(X.initializer)
+				# sess.run(X.initializer)
 				gen_loss_sum = np.float32(0)
 				dis_loss_sum = np.float32(0)
 				while True:
 					try:
-						X_data = sess.run(next_element)
+						# X_data = sess.run(next_element)
+						batch_xs, _ = data.train.next_batch(100)
+						batch_xs = tf.reshape(batch_xs, [100, 28, 28, 1])
 						z_data = np.random.uniform(-1, 1, (batch_size, self.z_dim))
-						train_fd = { z: z_data, x_data: X_data, K.learning_phase(): 1 }
-						_, gen_loss_val = sess.run([gen_train_step, gen_loss], feed_dict=train_fd)
+						train_fd = { z: z_data, x_data: batch_xs, K.learning_phase(): 1 }
+						_, gen_loss_val, y_val = sess.run([gen_train_step, gen_loss, y_pred1], feed_dict=train_fd)
 						_, dis_loss_val = sess.run([dis_train_step, dis_loss], feed_dict=train_fd)
 						gen_loss_sum += gen_loss_val
 						dis_loss_sum += dis_loss_val
+						print(y_val)
 					except tf.errors.OutOfRangeError:
 						break
 				print("\tepoch, gen_loss, dis_loss = %6d: %6.3f, %6.3f" % (epoch+1, gen_loss_sum, dis_loss_sum))
+
+		# with tf.train.MonitoredTrainingSession(
+		# 	checkpoint_dir='./out/model',
+		# 	hooks=[Hoge()]
+		# 	) as sess:
+		# 	while not sess.should_stop():
+		# 		gen_loss_sum = np.float32(0) # callbackで，epoch beforeで処理する
+		# 		dis_loss_sum = np.float32(0)
+		# 		X_data = sess.run(next_element)
+		# 		z_data = np.random.uniform(-1, 1, (batch_size, self.z_dim))
+		# 		train_fd = { z: z_data, x_data: X_data, K.learning_phase(): 1 }
+		# 		_, gen_loss_val = sess.run([gen_train_step, gen_loss], feed_dict=train_fd)
+		# 		_, dis_loss_val = sess.run([dis_train_step, dis_loss], feed_dict=train_fd)
+		# 		gen_loss_sum += gen_loss_val
+		# 		dis_loss_sum += dis_loss_val
 
 				# if plotting and epoch%DUMP_NUM == 0:
 				# 	saver = tf.train.Saver()
